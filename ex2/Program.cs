a79252d2fa5fe98af349b7a0db848afee49430d2
S00187348 <S00187348@LAB-B1042-33A>
2020-02-10 16:03:19 +0000

    finished ex1 and half of ex2

---
+++ ex2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = new DirectoryInfo("C:\\Windows").GetFiles();
+            var query = from item in files
+                        where item.Length > 10000
+                        orderby item.Length, item.Name
+                        select new MyFileInfo
+                        {
+                            name = item.Name,
+                            length = item.Length,
+                            CreationTime = item.CreationTime
+                        };
+
+            Console.WriteLine("Filename\tSize\t\tCreation Date");
+
+            foreach(MyFileInfo item in query)
+            {
+                Console.WriteLine(
+                    "{0} \t{1} bytes, \t{2}",
+                    item.name,item.length,item.CreationTime);
+
+            }
+
+            //Console.WriteLine(item);
+
+        }// endFile
+
+        public class MyFileInfo
+        {
+            public string name { get; set; }
+            public long length { get; set; }
+            public DateTime CreationTime { get; set; }
+
+            public override string ToString()
+            {
+                return string.Format("{0, -30}{1:F0} MB\T{2}",Name, length/10000,CreationTime)
+            }
+
+        }
+
+
+    }
+}
--- lab1/Program.cs
+++ lab1/Program.cs
@@ -4,12 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace lab1
+namespace LINQ_Array
 {
-    class Program
+    class program
     {
         static void Main(string[] args)
         {
+
+            NumberQuery();
+            numberLambda();
         }
+         
+        private static void numberLambda()
+        {
+            int[] Numbers = { 1, 5, 3, 6, 11, 2, 15, 21, 13, 12, 10 };
+
+            var OutPutNumbers = Numbers.Where(n => n > 5).OrderByDescending(n => n);
+
+            foreach(int number in OutPutNumbers )
+            {
+                Console.WriteLine(number.ToString());
+
+            }
+            Console.ReadLine();
+
+        }
+        
+        private static void NumberQuery()
+        {
+            int[] Numbers = { 1, 5, 3, 6, 11, 2, 15, 21, 13, 12, 10 };
+             var OutPutNumbers = from number in Numbers
+                                    where number > 5
+                                     orderby number
+                                     select number;
+
+            foreach (int number in Numbers)
+            {
+
+                Console.WriteLine(number.ToString());
+            }
+
+            Console.ReadLine();
+
+
+
+        }
+
+
     }
 }
